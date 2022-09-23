77befd70b5ef3f79591bdcb498ec75dd55014cef
Ikem Krueger <ikem.krueger@gmail.com>
2022-09-23 22:04:41 +0200

    Update kvberlin-arzt-und-psychotherapeutensuche.js

--- src/kvberlin-arzt-und-psychotherapeutensuche.js
+++ src/kvberlin-arzt-und-psychotherapeutensuche.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Kassenärztlich Vereinigung Berlin Arzt und Psychotherapeutensuche
+// @name         Kassenärztlich Vereinigung Berlin Arzt- und Psychotherapeutensuche
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Versteckt Therapeuten ohne Telefonnumer; hebt die aktuellen Sprechzeiten hervor
