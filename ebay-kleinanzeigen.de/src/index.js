109a6782fae60dae51fb1f02da1164112d03e10a
Ikem Krueger <ikem.krueger@gmail.com>
2022-09-23 21:48:14 +0200

    Update index.js

--- ebay-kleinanzeigen.de/src/index.js
+++ ebay-kleinanzeigen.de/src/index.js
@@ -1,5 +1,19 @@
-document.querySelectorAll(".ad-deleted").forEach((n, i) => {
-  setTimeout(() => {
-    n.querySelector(".watchlist-remove").click()
-  }, i * 1000, n);
-});
+// ==UserScript==
+// @name         eBay Kleinanzeigen
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Entfernt alle verkauften Artikel von der Merkliste.
+// @author       Ikem Krueger
+// @match        https://www.ebay-kleinanzeigen.de/m-merkliste.html
+// @grant        none
+// ==/UserScript==
+
+(function() {
+ 'use strict';
+
+  document.querySelectorAll(".ad-deleted").forEach((n, i) => {
+    setTimeout(() => {
+      n.querySelector(".watchlist-remove").click()
+    }, i * 1000, n);
+  });
+})();
