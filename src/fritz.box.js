ef0fe7fa034417f02804799a18b6bea4a26a0161
Ikem Krueger <ikem.krueger@gmail.com>
2022-10-13 01:01:01 +0200

    Update fritz.box.js

--- src/fritz.box.js
+++ src/fritz.box.js
@@ -1,3 +1,7 @@
+function inRange(testval, min, max) {
+  return min <= testval && testval <= max;
+}
+
 var einstufung = {
   SOFORTIGE_BESEITIGUNG: "#FF0000",
   BESEITIGUNG_BINNEN_MONATSFRIST: "#FFF200",
@@ -15,14 +19,36 @@ DsDocsis30Table.querySelector(".flexTableBody").querySelectorAll(".flexRow").for
 
   const pegel = parseFloat(powerLevel.textContent);
 
-  let hintergrundfarbe = einstufung.SOFORTIGE_BESEITIGUNG;
+  let hintergrundfarbe;
 
-  if(pegel >= -15 && pegel <= 15) {
-    hintergrundfarbe = einstufung.BESEITIGUNG_BINNEN_MONATSFRIST;
-  }
+  if(typ.textContent == "64QAM") {
+    if(inRange(pegel, -60.0, -14.0)) {
+      hintergrundfarbe = einstufung.SOFORTIGE_BESEITIGUNG;
+    }
 
-  if(pegel >= -8 && pegel <= 8) {
-    hintergrundfarbe = einstufung.TOLERIERTE_ABWEICHUNG;
+    if(inRange(pegel, -13.9, -12.0)) {
+      hintergrundfarbe = einstufung.BESEITIGUNG_BINNEN_MONATSFRIST;
+    }
+
+    if(inRange(pegel, -11.9, -10.0)) {
+      hintergrundfarbe = einstufung.TOLERIERTE_ABWEICHUNG;
+    }
+
+    if(inRange(pegel, -9.9, 7.0)) {
+      hintergrundfarbe = einstufung.REGELKONFORM;
+    }
+
+    if(inRange(pegel, 7.1, 12.0)) {
+      hintergrundfarbe = einstufung.TOLERIERTE_ABWEICHUNG;
+    }
+
+    if(inRange(pegel, 12.1, 14.0)) {
+      hintergrundfarbe = einstufung.BESEITIGUNG_BINNEN_MONATSFRIST;
+    }
+
+    if(inRange(pegel, 14.1, 60)) {
+      hintergrundfarbe = einstufung.SOFORTIGE_BESEITIGUNG;
+    }
   }
 
   row.style.backgroundColor = hintergrundfarbe;
