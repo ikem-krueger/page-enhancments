17b161c15088c9b7750f3cc025bc50802cd4baae
Ikem Krueger <ikem.krueger@gmail.com>
2022-10-21 23:53:54 +0200

    Update fritz.box.js

--- src/fritz.box.js
+++ src/fritz.box.js
@@ -1,11 +1,4 @@
-const COLORS = {
-  "Sofortige Beseitigung": "#FF0000",
-  "Beseitigung binnen Monatsfrist": "#FFF200",
-  "Tolerierte Abweichung": "#ADFF2F",
-  "Regel- / Vorgabekonform": "#00FF00"
-}
-
-const CLASSIFICATION_LEVEL = ["Sofortige Beseitigung", "Beseitigung binnen Monatsfrist", "Tolerierte Abweichung", "Regel- / Vorgabekonform", "Tolerierte Abweichung", "Beseitigung binnen Monatsfrist", "Sofortige Beseitigung"];
+const COLORS = ["#FF0000", "#FFF200", "#ADFF2F", "#00FF00", "#ADFF2F", "#FFF200", "#FF0000"];
 
 const RECEIVING_LEVEL = {
   "64QAM": [[-60.0, -14.0], [-13.9, -12.0], [-11.9, -10.0], [-9.9, 7.0], [7.1, 12.0], [12.1, 14.0], [14.1, 60.0]],
@@ -20,7 +13,7 @@ const TRANSMISSION_LEVEL = {
   "4K": [[-60.0, 38.0], [38.1, 40.0], [40.1, 44.0], [44.1, 47.0], [47.1, 48.0], [48.1, 50.0], [50.1, 60.0]]
 }
 
-function inRange(value, range) {
+function isInRange(value, range) {
   const [min, max] = range;
 
   return min <= value && value <= max;
@@ -33,17 +26,17 @@ function colorizePowerLevel(table, level) {
     const typ = childNodes[2];
     const powerLevel = childNodes[4];
 
-    let classification;
+    let color;
 
     level[typ.textContent].forEach((range, index) => {
-      if(inRange(parseFloat(powerLevel.textContent), range)) {
-        classification = CLASSIFICATION_LEVEL[index];
+      if(isInRange(parseFloat(powerLevel.textContent), range)) {
+        color = COLORS[index];
 
         return;
       }
     });
 
-    powerLevel.style.color = COLORS[classification];
+    powerLevel.style.color = color;
   });
 }
 
